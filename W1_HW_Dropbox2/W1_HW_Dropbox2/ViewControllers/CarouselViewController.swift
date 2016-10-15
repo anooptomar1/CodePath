5acd12966b0aae620dea8e934f755456388cd799
Daniel Lin <xdaniellin@ucla.edu>
2016-10-14 19:27:10 -0700

    W1 HW: Milestone #10 (Optional)
    Optional: Scrolling through the welcome screens instead of just tapping
    them.
    
    Create a custom free form view controller that is wide enough for 3
    screens. Creating Custom View Controllers, Creating a Free Form View
    Controller
    Add a UIScrollView with paging enabled.
    Add 3 UIImageViews for the welcome screens.

---
+++ W1_HW_Dropbox2/W1_HW_Dropbox2/ViewControllers/CarouselViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CarouselViewController.swift
+//  W1_HW_Dropbox2
+//
+//  Created by Daniel Lin on 10/14/16.
+//  Copyright © 2016 Daniel Lin. All rights reserved.
+//
+
+import UIKit
+
+class CarouselViewController: UIViewController {
+    @IBOutlet weak var carouselImage: UIImageView!
+    @IBOutlet weak var carouselScroll: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        carouselScroll.contentSize = CGSize(width:1095, height: 667)//carouselImage.frame.size
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
