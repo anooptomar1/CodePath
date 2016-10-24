ec369d6f97d766f30f5a4a1d4e9bef82489121e5
Daniel Lin <xdaniellin@ucla.edu>
2016-10-23 18:30:55 -0700

    W2 HW: Optional Milestone #1 (Landing Screen)

--- W2_HW_Carousel/W2_HW_Carousel/ViewControllers/IntroViewController.swift
+++ W2_HW_Carousel/W2_HW_Carousel/ViewControllers/IntroViewController.swift
@@ -8,13 +8,69 @@
 
 import UIKit
 
-class IntroViewController: UIViewController {
+class IntroViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var tile1: UIImageView!
+    @IBOutlet weak var tile2: UIImageView!
+    @IBOutlet weak var tile3: UIImageView!
+    @IBOutlet weak var tile4: UIImageView!
+    @IBOutlet weak var tile5: UIImageView!
+    @IBOutlet weak var tile6: UIImageView!
+        
+    // Arrays of initial transform values for tiles
+    var yOffsets  : [CGFloat] = [-335, -320, -495, -448, -610, -570]
+    var xOffsets  : [CGFloat] = [ -90,   35,   66,   80, -140,  -95]
+    var scales    : [CGFloat] = [   1, 1.75,  1.8,  1.9, 1.75, 1.75]
+    var rotations : [CGFloat] = [ -10,  -10,   10,   10,   10,  -10]
+    
+    var final_yOffsets  : [CGFloat] = []
+    var final_xOffsets  : [CGFloat] = []
+    var final_scales    : [CGFloat] = []
+    var final_rotations : [CGFloat] = []
+    var tiles : [UIImageView] = []
+    
+    //var tx: Double!
+    var ty: Double!
+    var scale: Double!
+    var rotation: Double!
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let start  = CGFloat(-20)
+        let end    = CGFloat(673)
+        let offset = CGFloat(scrollView.contentOffset.y)
+        let numberOfTiles = tiles.count
+        
+        for i in 0 ..< numberOfTiles
+        {
+            //Get Current Tile
+            let tile = tiles[i]
+            
+            //Calculate Values
+            let tx = convertValue(inputValue: offset, r1Min: start, r1Max: end, r2Min: xOffsets[i], r2Max: 0)
+            let ty = convertValue(inputValue: offset, r1Min: start, r1Max: end, r2Min: yOffsets[i], r2Max: 0)
+            let scale = convertValue(inputValue: offset, r1Min: start, r1Max: end, r2Min: scales[i], r2Max: 1)
+            let rotation_radians = convertValue(inputValue: offset, r1Min: CGFloat(start), r1Max: CGFloat(end), r2Min: rotations[i], r2Max: 0)
+            let rotation_degrees = CGFloat(Double(rotation_radians) * M_PI / 180)
+            
+            //Apply Transform
+            tile.transform = CGAffineTransform(translationX: CGFloat(tx),y: CGFloat(ty))
+            tile.transform = tile.transform.scaledBy(x: scale, y: scale)
+            tile.transform = tile.transform.rotated(by: rotation_degrees)
+        }
+        
+        print(scrollView.contentOffset.y)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         scrollView.contentSize = imageView.frame.size
+        scrollView.delegate = self
+        
+        tiles = [tile1, tile2, tile3, tile4, tile5, tile6]
         
         // Do any additional setup after loading the view.
     }
