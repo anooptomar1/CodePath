a4cdaabec217c7684158324e8a8f5ee2771c228f
Daniel Lin <xdaniellin@ucla.edu>
2016-11-10 17:33:13 -0800

    dump

---
+++ W5_L1_Gesture/W5_L1_Gesture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W5_L1_Gesture
+//
+//  Created by Daniel Lin on 11/8/16.
+//  Copyright © 2016 Daniel Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
