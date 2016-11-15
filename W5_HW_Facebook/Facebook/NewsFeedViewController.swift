294308315f738ef39b4047c4e62a28181f403b94
Daniel Lin <xdaniellin@ucla.edu>
2016-11-14 20:24:47 -0800

    Milestone 2: Basic Layout and Transition
    Basic Layout and Transition
    In the Storyboard, add the 5 UIImageViews to the feed, remember to
    enable user interaction for each image view.
    Add tap gesture recognizers to each UIImageView.
    Create a PhotoViewController that has an image view embedded in a
    scroll view and a Done button.
    Create a modal segue from the FeedViewController to the
    PhotoViewController and give it a unique identifier.
    Attach an action to each gesture recognizer (they can be connected to
    the same action method). In the action, perform the segue. Guide:
    Performing Manual Segue.
    Set the image of the PhotoViewController. Guide: Passing Data in Segues
    Connect the Done button to an action that dismisses the view controller.

---
+++ W5_HW_Facebook/Facebook/NewsFeedViewController.swift
@@ -0,0 +1,49 @@
+//
+//  NewsFeedViewController.swift
+//  Facebook
+//
+//  Created by Timothy Lee on 8/3/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var feedImageView: UIImageView!
+    var selectedImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure the content size of the scroll view
+        scrollView.contentSize = CGSize(width: 320, height: feedImageView.image!.size.height)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        scrollView.contentInset.top = 0
+        scrollView.contentInset.bottom = 50
+        scrollView.scrollIndicatorInsets.top = 0
+        scrollView.scrollIndicatorInsets.bottom = 50
+    }
+    
+    
+    @IBAction func didTapImage(_ sender: UITapGestureRecognizer) {
+        let selectedImageView = sender.view as! UIImageView
+        selectedImage = selectedImageView.image
+        performSegue(withIdentifier: "imageSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let imageViewController = segue.destination as! ImageViewController
+        imageViewController.image = selectedImage
+    }
+}
